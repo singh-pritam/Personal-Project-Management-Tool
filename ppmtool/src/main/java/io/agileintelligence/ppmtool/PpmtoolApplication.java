0c2e3c6859a586416a797e19c52ac33a4830cd86
pritam_thoughtworks <pritam.singh@thoughtworks.com>
2022-08-28 17:25:30 +0530

    Introduced project object

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/PpmtoolApplication.java
@@ -1,13 +1,10 @@
 package io.agileintelligence.ppmtool;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PpmtoolApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PpmtoolApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolApplication.class, args);
+    }
 }
